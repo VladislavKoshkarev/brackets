b529b384145c0b6a58d7939d9b8dc6b707e39943
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2023-02-08 13:24:03 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+  for (let char = 0; char < str.length; char++) {
+    console.log(stack)
+    let currentChar = str[char]
+    bracketsConfig.forEach(arr => {
+      if ((currentChar === arr[1]) && stack.length === 0) stack.push(currentChar)
+      else if ((currentChar === arr[1]) && stack[stack.length - 1] === arr[0]) {stack.pop()}
+      else {
+        if ((currentChar === arr[0])) stack.push(currentChar)
+        else if (stack.length === 0) return false;
+      }
+      })
+  }
+  return (stack.length === 0)
 }
